223a535b5b2a76a4fcd96b7e18273afc7eca7a18
vanderMerwe99 <53337072+vanderMerwe99@users.noreply.github.com>
2020-11-17 09:19:37 +0000

    Changed Routing

--- DataClassifierAPI/Controllers/LoginController.cs
+++ DataClassifierAPI/Controllers/LoginController.cs
@@ -14,7 +14,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace DataClassifierAPI.Controllers
 {
-    [Route("https://vandermerwe99.github.io/DataClassifierAPI//[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
--- DataClassifierAPI/Controllers/WeatherForecastController.cs
+++ DataClassifierAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace DataClassifierAPI.Controllers
 {
     [ApiController]
-    [Route("https://vandermerwe99.github.io/DataClassifierAPI/[controller]")]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
